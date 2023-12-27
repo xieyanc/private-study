1b7e51b814511251c9564aa17777d48c15957a0c
dianxi <1756303953@qq.com>
2023-12-27 17:16:53 +0800

    feature: 初识无界

--- WuJie/main/src/main.ts
+++ WuJie/main/src/main.ts
@@ -2,13 +2,13 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import Wujie from 'wujie-vue3' // 引入无界作者封装的函数进行应用的注册
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia()).use(router).use(Wujie) // use一下
 
 app.mount('#app')
